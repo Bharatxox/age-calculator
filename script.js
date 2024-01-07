fe0ef742043e1dd743ca84acb36462ddaeeb32a3
Bharat Bisht <kamalbisht819@gmail.com>
2024-01-07 15:46:19 +0530

    age cal

---
+++ script.js
@@ -0,0 +1,28 @@
+function ageCalculator() {
+    var userinput = document.getElementById("DOB").value;
+    var dob = new Date(userinput);
+    if(userinput==null || userinput=='') {
+        alert('Entre the valid data'); 
+      return false; 
+    } 
+    else {
+    
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+    
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff); 
+    
+    //extract year from date    
+    var year = age_dt.getUTCFullYear();
+    
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+    
+    //display the calculated age
+    return document.getElementById("age").innerHTML =  
+            age;
+    }
+}
+const btn = document.querySelector("#click");
+btn.addEventListener("click",ageCalculator);
